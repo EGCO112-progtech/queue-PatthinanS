fc91055764461ec598c79ac14786a55c23bc7100
PS <156405238+PatthinanS@users.noreply.github.com>
2024-02-23 10:47:23 +0000

    kk

--- main.c
+++ main.c
@@ -14,22 +14,27 @@ int main(int argc , char **argv) {
    q.tailPtr=NULL;
    q.size=0;
 
-   int i,x;
+   int i,price,qty,n;
 
   //  enqueue(&headPtr, &tailPtr, 5);
   //  printf("%d\n", dequeue(&headPtr, &tailPtr));
    
 
  for(i=1;i<argc;i++){
-        int n = atoi(argv[i]);
+      int n, qty, c=0;
         if(strcmp(argv[i],"x")==0){
-            x=dequeue_struct(&q);
-            printf("dequeing %d\n",x);
+            price = dequeue_struct(&q);
+            c++;
+            change(price);
         }
-        else {
-       enqueue_struct(&q, n);
-       printf("%d\n", n);
-           
+        else{
+        n=atoi(argv[i]);
+          if(strcmp(argv[i+1],"x")==0) qty=1; //case order followed by a fucking x because it will be a bug you mtfk
+          else qty=atoi(argv[i+1]); //normal case
+
+          enqueue_struct(&q, n, qty);
+          i++;
+        printf("My order is %d, qty %d\n", n, qty);
         }
  }
   return 0;
